2b57d80b2c5a26dbbe00b4516ec4a9473b9111bb
Dorian Neto <doriansampaioneto@gmail.com>
2017-01-30 11:37:40 -0300

    feat(user-module): routes that not be using removed

--- web/routes/web.php
+++ web/routes/web.php
@@ -9,5 +9,5 @@ Route::group(['middleware' => 'auth'], function () {
     Route::get('users/trash', ['as' => 'users.trash', 'uses' => 'UserController@trash']);
     Route::get('users/restore/{id}', ['as' => 'users.restore', 'uses' => 'UserController@restore']);
     Route::delete('users/trash/{id}', ['as' => 'users.force_delete', 'uses' => 'UserController@forceDelete']);
-    Route::resource('users', 'UserController');
+    Route::resource('users', 'UserController', ['except' => ['create', 'store', 'show']]);
 });
