34b4c6b5ad2bd691f0adc73cc5c6fda5a57d536c
Dorian Neto <doriansampaioneto@gmail.com>
2017-01-30 11:19:30 -0300

    feat(404): page added

---
+++ web/resources/views/errors/404.blade.php
@@ -0,0 +1,18 @@
+@include('layout.include.header')
+
+    <div class="container">
+      <div id="showtime">Page 404</div>
+    </div><!-- /container -->
+
+    <!-- js placed at the end of the document so the pages load faster -->
+    <script src="assets/js/jquery.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+
+    <!--BACKSTRETCH-->
+    <!-- You can use an image of whatever size. This script will stretch to fit in any screen size.-->
+    <script type="text/javascript" src="assets/js/jquery.backstretch.min.js"></script>
+    <script>
+        $.backstretch("assets/img/login-bg.jpg", {speed: 500});
+    </script>
+  </body>
+</html>
